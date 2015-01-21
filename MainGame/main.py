8b62324722b5cc84e18c40efd014e5fc6c1b1b4b
Mary Kris P. Morco <marykris.morco@bicol-u.edu.ph>
2015-01-21 09:34:51 +0800

    Update main.py

--- MainGame/main.py
+++ MainGame/main.py
@@ -22,11 +22,11 @@ def Hamster(x,y):
     gameDisplay.blit(HamImg,(x,y))
 
 def text_objects(text, font):
-    TextSurface = font.render(text, True, black)
-    return TextSurface.get_rect()
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
 
 def message_display(text):
-    largeText = pygame.font.SysFont(None, 25)
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((display_width/2),(display_height/2))
     gameDisplay.blit(TextSurf, TextRect)
