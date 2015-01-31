b27d9336034f47c51f6648d4f760b84760cd293c
Bianca Denise del Puerto <biancadenise.delpuerto@bicol-u.edu.ph>
2015-01-31 20:54:45 +0800

    removed unnecessary line of codes

--- MainGame/main_v2.py
+++ MainGame/main_v2.py
@@ -22,25 +22,6 @@ hamster_height = 100
 def Hamster(x,y):
     gameDisplay.blit(HamImg,(x,y))
 
-def text_objects(text, font):
-    textSurface = font.render(text, True, black)
-    return textSurface, textSurface.get_rect()
-
-def message_display(text):
-    largeText = pygame.font.Font('freesansbold.ttf', 115)
-    TextSurf, TextRect = text_objects(text, largeText)
-    TextRect.center = ((display_width/2),(display_height/2))
-    gameDisplay.blit(TextSurf, TextRect)
-
-    pygame.display.update()
-
-    time.sleep(2)
-    
-    game_loop()
-
-def crash():
-    message_display('You Crashed')
-
 def terminate():
     pygame.quit()
     quit()
