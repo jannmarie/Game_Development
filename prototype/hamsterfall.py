cc07300bf03bbf90df0637c5d2eb82edb68c2765
Jann Marie Cabug <jannmarie.cabug@bicol-u.edu.ph>
2015-02-20 23:16:16 +0800

    collision detection

--- prototype/hamsterfall.py
+++ prototype/hamsterfall.py
@@ -1,78 +1,57 @@
 import pygame
 import random
-import time
 
 black = (0,0,0)
-blue = (40,190,200)
+blue = (40,190,200) 
 red = (255, 0, 0)
 white = (255, 255, 255)
-
+         
 class Block(pygame.sprite.Sprite):
     def __init__(self, color = blue, width = 100, height = 100):
         super(Block, self).__init__()
         self.image = pygame.Surface((width, height))
-        self.x = window_width/2
-        self.y = window_height/2 - 200
+        self.image.fill(color)
         self.rect = self.image.get_rect()
         self.sound = pygame.mixer.Sound("intro.wav")
+        self.hspeed = 0
+        self.vspeed = 0
+
+    def set_properties(self):
+        self.rect = self.image.get_rect()
+        self.origin_x = self.rect.centerx
+        self.origin_y = self.rect.centery
+
+    def change_speed(self, hspeed, vspeed):
+        self.hspeed += hspeed
+        self.vspeed += vspeed
+         
         
     def set_position(self, x, y):
         self.rect.x = x
         self.rect.y = y
+        
     def set_image(self, filename = None):
         if(filename != None):
             self.image = pygame.image.load(filename)
             self.rect = self.image.get_rect()
+            
     def play_sound(self):
         self.sound.play()
-    def handle_keys(self, collidable):
-                
-        key = pygame.key.get_pressed()
-        dist = 6 # distance moved in 1 frame, try changing it to 5
-        if key[pygame.K_DOWN]: # down key
-            self.y += dist # move down
-        elif key[pygame.K_UP]: # up key
-            self.y -= dist  # move up
-        if key[pygame.K_RIGHT]: # right key
-            self.x += dist # move right
-        elif key[pygame.K_LEFT]: # left key
-            self.x -= dist # move left
-
-        if self.x > window_width - 100:
-            self.x = window_width - 100
-        if self.x < 0:
-            self.x = 0
-        if self.y > window_height - 100:
-            self.y = window_height - 100
-        if self.y < 0:
-            self.y = 0
-            
-        if key[pygame.K_UP]:
-            if key[pygame.K_LEFT] or key[pygame.K_RIGHT]:
-                self.x = 0
-            if key[pygame.K_DOWN] or key[pygame.K_UP]:
-                self.y = 0
-
-        collision_list = pygame.sprite.spritecollide(self, collidable, False)
-        for collided_object in collision_list:
-            if(self.x > 0):
-                self.rect.right = collided_object.rect.left
-            elif(self.x < 0):
-                self.rect.left = collided_object.rect.right
-                
-        collision_list = pygame.sprite.spritecollide(self, collidable, False)
-        for collided_object in collision_list:
-            if(self.y > 0):
-                self.rect.bottom = collided_object.rect.top
-            elif(self.y < 0):
-                self.rect.top = collided_object.rect.bottom            
-            
         
-    def draw(self, surface):
-            """ Draw on surface """
-            # blit yourself at your current position
-            surface.blit(self.image, (self.x, self.y))
-            
+    def update(self):
+        self.rect.x += self.hspeed #for x                
+        self.rect.y += self.vspeed # for y
+        
+        if self.rect.x > window_width - 100:
+            self.rect.x = window_width - 100
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.y > window_height - 100:
+            self.rect.y = window_height - 100
+        if self.rect.y < 0:
+            self.rect.y = 0
+
+
 class Cloud(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
@@ -100,7 +79,8 @@ class Cloud(pygame.sprite.Sprite):
         # If block is too far down, reset to top of screen.
         if self.rect.y < -100:
             self.reset_pos()
-            
+   
+
 def set_message( text ):
     global message, previous_message
     message = font.render(text, True, black)
@@ -110,81 +90,88 @@ def set_message( text ):
 if ( __name__ == "__main__" ):
     pygame.init()
 
-    window_size = window_width, window_height = 800, 600
+    window_size = window_width, window_height = 800, 600 
     window = pygame.display.set_mode( window_size, pygame.RESIZABLE )
-
     pygame.display.set_caption("Hamster Fall")
-    window.fill(white)
+    window.fill(blue)
 
     clock = pygame.time.Clock()
     frames_per_second = 60
 
-    player_group = pygame.sprite.Group()
-    all_sprites_list = pygame.sprite.Group()
-    block_list = pygame.sprite.Group()
-
+    block_group = pygame.sprite.Group()
+    player = pygame.sprite.Group()
+ 
     for i in range(5):
-        # This represents a block
-        block = Cloud(black, 100, 100)
-        block.set_image("cloud.png")
-     
-        # Set a random location for the block
-        block.rect.x = random.randrange(0, window_width)
-        block.rect.y = random.randrange(window_height)
-        block_list.add(block)
-        
-     
-        # Add the block to the list of objects
-        all_sprites_list.add(block)
+        cloud = Cloud(red, 100, 100)
+        cloud.set_image("cloud.png")
+        cloud.rect.x = random.randrange(0, window_width)
+        cloud.rect.y = random.randrange(500, window_height+200)
+        block_group.add(cloud)
     for i in range(5):
-        block_two = Cloud(red, 100, 100)
-        block_two.set_image("cloud.png")
-        block_two.rect.x = random.randrange(0, window_width)
-        block_two.rect.y = random.randrange(window_height)
-        block_list.add(block_two)
-         
-
+        cloud_two = Cloud(red, 100, 100)
+        cloud_two.set_image("cloud.png")
+        cloud_two.rect.x = random.randrange(0, window_width)
+        cloud_two.rect.y = random.randrange(900, 1200)
+        block_group.add(cloud_two)
+    
+    
     a_block = Block()
     a_block.set_image("hammy.png")
-    a_block.set_position(window_width/2, window_height/2)
-    a_block.play_sound()
-    player_group.add(a_block)
-
-    font = pygame.font.SysFont("Arial,Times New Roman", 90)
+    a_block.set_position(window_width/2, 0)
+    player.add(a_block)
+#    block_group.add(a_block)
+   
+    
+    font = pygame.font.SysFont("Arial", 90)
     message = previous_message = None
     set_message("")
-
-
-    collidable_objects = pygame.sprite.Group()
-    collidable_objects.add(block)
+    a_block.play_sound()
 
     running = True
 
-
     while (running):
         for event in pygame.event.get():
-            if (event.type == pygame.QUIT):
+            if(event.type == pygame.QUIT):
                 running = False
                 
- 
-        window.fill(white)
-        a_block.handle_keys(collidable_objects)
-        if (pygame.sprite.groupcollide(block_list, player_group, None, None)):
-            set_message("There is a collision!")
-        else:
-            set_message("No collision!")
-            
-        if ( message != previous_message ):
-            set_message(message)
-            
-        window.blit(message, (window_width/2 - message.get_rect().width/2, window_height/2-100))
-            
-        a_block.draw(window)
-        block_list.update()
-        block_list.draw(window)
-
-        
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == pygame.K_LEFT):
+                    a_block.change_speed( -5, 0 )
+                if (event.key == pygame.K_RIGHT):
+                    a_block.change_speed( 5, 0 )
+                if (event.key == pygame.K_UP):
+                    a_block.change_speed( 0, -5 )
+                if (event.key == pygame.K_DOWN):
+                    a_block.change_speed( 0, 5 )
+            if (event.type == pygame.KEYUP):
+                if (event.key == pygame.K_LEFT):
+                    a_block.change_speed( 5, 0 )
+                if (event.key == pygame.K_RIGHT):
+                    a_block.change_speed( -5, 0 )
+                if (event.key == pygame.K_UP):
+                    a_block.change_speed( 0, 5 )
+                if (event.key == pygame.K_DOWN):
+                    a_block.change_speed( 0, -5 )
+   
         clock.tick(frames_per_second)
+        window.fill(blue)
+        a_block.update()
+        cloud.update()
+
+
+        if(pygame.sprite.spritecollide(a_block, block_group, False)):
+            window.fill(red)
+            set_message("There is a collision")
+        else:
+            set_message("  ")
+         
+        window.blit(message, (window_width/2 - message.get_rect().width/2, window_height/2-100))
+        player.update()
+        player.draw(window)
+        block_group.update()
+        block_group.draw(window)
+        
         pygame.display.update()
 
     pygame.quit()
+    
