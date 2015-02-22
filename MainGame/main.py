1d691aca9a1cb4ae9b18a5c7378f9994c5773844
Bianca Denise del Puerto <biancadenise.delpuerto@bicol-u.edu.ph>
2015-02-22 14:51:43 +0800

    transferred the prototype to MainGame

--- MainGame/main.py
+++ MainGame/main.py
@@ -2,125 +2,150 @@ import pygame
 import time
 import random
 
-pygame.init()
-
-display_width = 800
-display_height = 600
-
-black = (0,0,0)
 blue = (16,135,247)
 
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Hamster Fall')
-icon = pygame.image.load('hammy2.png')
-pygame.display.set_icon(icon)
+class Hamster(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super(Hamster, self).__init__()
+        self.image = pygame.Surface((width, height))
+        self.rect = self.image.get_rect()
+        self.width = width
+        self.height = height
+        self.hspeed = 0
+        self.vspeed = 0
 
-clock = pygame.time.Clock()
+    def change_speed(self, hspeed, vspeed):
+        self.hspeed += hspeed
+        self.vspeed += vspeed
 
-HamImg = pygame.image.load('hammy.png')
-hamster_width = 130
-hamster_height = 172
+    def set_position(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
 
-cloud = pygame.image.load('Cloud_1.png')
-cloud_width = 202
-cloud_height = 179 
+    def set_image(self, imagename):
+        self.image = pygame.image.load(imagename)
+        self.rect = self.image.get_rect()
 
-def Hamster(x,y):
-    gameDisplay.blit(HamImg,(x,y))
+#    def play_sound(self):
+#        self.sound.play()
 
-def Cloud(cloud_x,cloud_y):
-    gameDisplay.blit(cloud, (cloud_x,cloud_y))
-    
+    def update(self):
+        self.rect.x += self.hspeed            
+        self.rect.y += self.vspeed
+
+        if self.rect.x > screen_width - self.width:
+            self.rect.x = screen_width - self.width
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.y > screen_height - self.height:
+            self.rect.y = screen_height - self.height
+        if self.rect.y < 0:
+            self.rect.y = 0
+
+class BadCloud(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.width = width
+        self.height = height
+        self.rect = self.image.get_rect()
+
+    def reset_pos(self):
+        self.rect.y = 600
+        self.rect.x = random.randrange(0, screen_width)
+
+    def set_image(self, imagename):
+        self.image = pygame.image.load(imagename)
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.y -= 1
+        if self.rect.y < 0 - self.height:
+            self.reset_pos()
+
 def terminate():
     pygame.quit()
     quit()
 
-def game_loop():
-    x = (display_width * 0.45)
-    y = (display_height * 0.2)
+if( __name__ == "__main__" ):
+    pygame.init()
 
-    x_change = 0
-    y_change = 0
+    screen_width = 800
+    screen_height = 600
 
-    cloud_startx = random.randrange(0, display_width)
-    cloud_starty = 600
-    cloud_speed = 2
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    pygame.display.set_caption('Hamster Fall')
+    icon = pygame.image.load('hammy.png')
+    pygame.display.set_icon(icon)
 
-    bgOne = pygame.image.load('background_sample.png')
-    bgTwo = pygame.image.load('background_sample.png')
+    clock = pygame.time.Clock()
+    frames_per_second = 60
 
-    bgOne_y = 0
-    bgTwo_y = bgOne.get_height()
-    
-    gameExit = False
-    while not gameExit:
+    playergroup = pygame.sprite.Group()
+    badcloudsgroup = pygame.sprite.Group()
+
+    hammy = Hamster(130, 172)
+    hammy.set_image("hammy.png")
+    hammy.set_position(screen_width/2, 0)
+    playergroup.add(hammy)
+
+    for i in range(2):
+        badcloud1 = BadCloud(100, 100)
+        badcloud1.set_image("badcloud1.png")
+        badcloud1.rect.x = random.randrange((50-badcloud1.width), screen_width)
+        badcloud1.rect.y = screen_height
+        badcloudsgroup.add(badcloud1)
+    for i in range(3):
+        badcloud2 = BadCloud(202, 179)
+        badcloud2.set_image("badcloud2.png")
+        badcloud2.rect.x = random.randrange((50-badcloud1.width), screen_width)
+        badcloud2.rect.y = screen_height
+        badcloudsgroup.add(badcloud2)
         
+
+    running = True
+
+    while (running):
         for event in pygame.event.get():
-            #check for the QUIT event
-            if event.type == pygame.QUIT:
-                gameExit = True
+            if(event.type == pygame.QUIT):
+                running = False
                 terminate()
-                
-            #check for the directions events when keyboard is pressed    
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x_change = -5
-                if event.key == pygame.K_RIGHT:
-                    x_change = 5
-                if event.key == pygame.K_DOWN:
-                    y_change = 5
-                if event.key == pygame.K_UP:
-                    y_change = -5
                     
-            #check for the directions events when keyboard is released
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    x_change = 0
-                if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
-                    y_change = 0
+            if (event.type == pygame.KEYDOWN):
+                if (event.key == pygame.K_LEFT):
+                    hammy.change_speed( -5, 0 )
+                if (event.key == pygame.K_RIGHT):
+                    hammy.change_speed( 5, 0 )
+                if (event.key == pygame.K_UP):
+                    hammy.change_speed( 0, -5 )
+                if (event.key == pygame.K_DOWN):
+                    hammy.change_speed( 0, 5 )
 
-        x += x_change
-        y += y_change
-
-        gameDisplay.blit(bgOne, (0, bgOne_y))
-        gameDisplay.blit(bgTwo, (0, bgTwo_y))
-        
-
-        Cloud(cloud_startx, cloud_starty)
-        cloud_starty -= cloud_speed
-        
-        Hamster(x,y)
-
-        if x > display_width - hamster_width:
-            x = display_width - hamster_width
-        if x < 0:
-            x = 0
-        if y > display_height - hamster_height:
-            y = display_height - hamster_height
-        if y < 0:
-            y = 0
-
-        if cloud_starty < 0 - cloud_height:
-            cloud_starty = 600
-            cloud_startx = random.randrange(0, display_width)
-
-#Collision Detector
-
-#1-------------------------------A------------------------|   |-----------------------------------D------------------------------------------|  |----------------------------------------------------------B---------------|    |----------------------------------C------------------------------------------| #2|-----------------------------------------A-------------------------------|    |-----------------------------------D--------------------------------------|   |---------------------------B---------------------|   |-----------------------------------C-----------------------------------------|
-        if x>=cloud_startx and x<=cloud_startx+cloud_width and y+hamster_height>=cloud_starty and y+hamster_height<=cloud_starty+cloud_height or x+hamster_width>=cloud_startx and x+hamster_width<=cloud_startx+cloud_width and y+hamster_height>=cloud_starty and y+hamster_height<=cloud_starty+cloud_height or cloud_startx+cloud_width>=x and cloud_startx+cloud_width<=x+hamster_width and cloud_starty+cloud_height>=y and cloud_starty+cloud_height<=y+hamster_height or cloud_startx>=x and cloud_startx<=x+hamster_width and cloud_starty+cloud_height>=y and cloud_starty+cloud_height<=y+hamster_height:
-            terminate()
-
-        bgOne_y -= 2
-        bgTwo_y -= 2
-        
-        if bgOne_y == -1 * bgOne.get_height():
-            bgOne_y = bgTwo_y + bgTwo.get_height()
-        if bgTwo_y == -1 * bgTwo.get_height():
-            bgTwo_y = bgOne_y + bgOne.get_height()
+            if (event.type == pygame.KEYUP):
+                if (event.key == pygame.K_LEFT):
+                    hammy.change_speed( 5, 0 )
+                if (event.key == pygame.K_RIGHT):
+                    hammy.change_speed( -5, 0 )
+                if (event.key == pygame.K_UP):
+                    hammy.change_speed( 0, 5 )
+                if (event.key == pygame.K_DOWN):
+                    hammy.change_speed( 0, -5 )
             
-        pygame.display.update()
-        
-        clock.tick(60)
 
-game_loop()
+        clock.tick(frames_per_second)
+        screen.fill(blue)
+        hammy.update()
+        badcloud1.update()
+        badcloud2.update()
+
+        if(pygame.sprite.spritecollide(hammy, badcloudsgroup, False)):
+            running = False
+            terminate()
+        
+        playergroup.update()
+        playergroup.draw(screen)
+        badcloudsgroup.update()
+        badcloudsgroup.draw(screen)
+                
+        pygame.display.update()
 
